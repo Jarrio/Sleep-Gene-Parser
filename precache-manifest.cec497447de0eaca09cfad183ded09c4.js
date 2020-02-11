866450365ae6123c5cb7a5356c328ca0b89e91e8
Jarrio <Jarrio@users.noreply.github.com>
2020-02-11 19:06:01 +0000

    Updates

---
+++ precache-manifest.cec497447de0eaca09cfad183ded09c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d9472bca1baa6435bc0cf95295e9261",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93a847849629b933fa6d",
+    "url": "/static/js/2.c367e0b2.chunk.js"
+  },
+  {
+    "revision": "9373cde837f1a52541cff3e378761c7d",
+    "url": "/static/js/2.c367e0b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "479b0fee0f3af177e731",
+    "url": "/static/js/main.7c3a11cc.chunk.js"
+  },
+  {
+    "revision": "a6d2fc2fa5f9a20c106e",
+    "url": "/static/js/runtime-main.20dc1d8c.js"
+  }
+]);
